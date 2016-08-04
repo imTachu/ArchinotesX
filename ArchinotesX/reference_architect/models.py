49d0e62ebc25ab5ad959befa76543e55f944847e
Tachú <tachu.salamanca@gmail.com>
2016-08-04 17:07:20 -0500

    autenticación v2

--- ArchinotesX/reference_architect/models.py
+++ ArchinotesX/reference_architect/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name

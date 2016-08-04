40c79c0e755c13927604e6bf59e3bd74b14d3783
Tachú <tachu.salamanca@gmail.com>
2016-08-03 19:33:42 -0500

    Landing page

--- ArchinotesX/reference_architect/views.py
+++ ArchinotesX/reference_architect/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Company,SQLSource
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the reference-architect index.")
+    template = loader.get_template('reference_architect/index.html')
+    return HttpResponse(template.render(request))
+
